6966b7af5f25efc8639afee30c620cfa9f5c5df5
DESKTOP-G83JB5E\nguye <nguyenvannui120899@gmail.com>
2021-05-02 15:48:43 +0700

    Mountain first

---
+++ src/SKN.Frontend/src/Component/FooterComp.js
@@ -0,0 +1,9 @@
+
+
+export default function FooterComp(props){
+    return(
+        <div className="footer">
+            Copy Rights Reserved by <span>Học tiếng anh Online</span>
+        </div>
+    )
+}
